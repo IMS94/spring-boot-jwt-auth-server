fd1bb60b23bc85a9d16adb287913dddd1d007e78
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2022-03-27 10:28:39 +0530

    Create spring initialzr project

---
+++ src/main/java/com/example/springboot/jwt/JwtAuthIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthIdentityProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthIdentityProviderApplication.class, args);
+	}
+
+}
